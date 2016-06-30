6ffebce51546fcbd7b8b73fb811412a9a6f8a271
Vendea <kbeine@live.com>
2016-06-30 15:00:10 -0400

    modules

--- Tools/Incrementer.py
+++ Tools/Incrementer.py
@@ -3,20 +3,12 @@ Created on Jun 27, 2016
 
 @author: KatherineMJB
 '''
+
+import tensorflow as tf
 class Incrementer:
-    
-    import tensorflow as tf
     
     def __init__(self, base):
         self.base = base
-        
-    '''def run(self, tens):
-        ret = []
-        for i in range(0, len(arr), 2):
-            carry, place = self.ex(arr[i], arr[i+1]) 
-            ret.append(carry)
-            ret.append(place)
-        return ret'''
-        
+ 
     def ex(self, l, r):
-        return tf.div(tf.add(l, r), base), tf.mod(tf.add(l,r), base)
+        return tf.div(tf.add(l, r), self.base), tf.mod(tf.add(l,r), self.base)
