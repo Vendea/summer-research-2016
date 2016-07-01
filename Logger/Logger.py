e71e6d20769d3d91a786244f0e008b995277e3b6
rexmagi <tdhst231@mail.rmu.edu>
2016-07-01 13:24:39 -0400

    updated logger

--- Logger/Logger.py
+++ Logger/Logger.py
@@ -3,16 +3,17 @@ import datetime
  
 
 class DataLogger:
-	def __init__(self,ExperimentTitle,layers,nodes):
-		current_time = datetime.datetime.now().time()
+	def __init__(self,ExperimentTitle,layers,nodes,testing,header):
 		self.f = open(
-			ExperimentTitle+"-"+str(current_time.isoformat())+".csv",
+			ExperimentTitle+".csv",
 			'w')
 		self.f.write("Layers: "+str(layers)+"\n")
 		self.f.write("Nodes: "+str(nodes)+"\n")
 		self.f.write("========================================================"+"\n")
-		self.f.write("Epoch,Cost,Computation_Time,Train_Accuracy,Test_Accuracy"+"\n")
+		self.f.write(header+"\n")
 		self.f.flush()
-	def writeData(self,e,trainc,testc,t,trainacu,testacu):
-		self.f.write(str(e)+","+str(trainc)+","+str(testc)+","+str(t)+","+str(trainacu)+","+str(testacu)+"\n")
+	def writeData(self,cols):
+		data = str(cols)[1:]
+		data = data[:-1]
+		self.f.write(data+"\n")
 		self.f.flush()
