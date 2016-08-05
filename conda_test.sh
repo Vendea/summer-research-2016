55dd74d7bd527607350ef59a8aa30d8a4637bfbb
rexmagi <tdhst231@mail.rmu.edu>
2016-08-04 22:27:33 -0400

    conda test

--- conda_test.sh
+++ conda_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --ntasks=8
+#SBATCH --ntasks=1
 #SBATCH -t 00:00:30
 #SBATCH --gres=gpu
 . ~/.profile
@@ -9,7 +9,6 @@ module load gcc/6.1.0
 module load openmpi/1.10.2
 module load cuDNN/v4.0
 module load cuda/7.5.18
-unsetenv PYTHONHOME
-. ~/.python/python/bin/activate
+source ~/anaconda2/bin/activate
 
-mpiexec  python -m mpi4py helloworld
+mpirun python deep_test.py
